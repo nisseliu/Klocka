fbaf33318ad9f02fd6a47712c6b9e6e4ccd6b2f7
Nisseliu <nilba239@student.liu.se>
2021-01-21 16:02:32 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Klocka.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Klocka.dir/main.cpp.obj"
+  "Klocka.exe"
+  "Klocka.exe.manifest"
+  "Klocka.lib"
+  "Klocka.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Klocka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    class Time {
+    public:
+        //Increase
+        //Decrease
+        //Compare
+        //Check tar in en int som returnerar bool
+        //Output
+        //Get variabel
+    private:
+        int secconds;
+        int minutes;
+        int Hours;
+    };
+
+
+    return 0;
+}
+ 
